64eecb8674c42ba279b6840a4680faa7f005c806
Nodar Miminoshvili <miminoshvilinodar@gmail.com>
2024-10-12 18:01:03 +0400

    Add homepage redirect for scroll-to-section buttons

--- app/components/Header/ScrollToSectionButton.tsx
+++ app/components/Header/ScrollToSectionButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { usePathname, useRouter } from 'next/navigation';
+
 type Props = {
   title: string;
   sectionId: string;
@@ -7,9 +9,23 @@ type Props = {
 };
 
 const ScrollToSectionButton = ({ sectionId, position, title }: Props) => {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   return (
     <button
-      onClick={() => {
+      className="transition-colors hover:text-[--accent-color]"
+      onClick={async () => {
+        if (pathname !== '/') {
+          replace('/');
+          const timeoutAfterNavigation: Promise<void> = new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+            }, 250);
+          });
+          await timeoutAfterNavigation;
+        }
+
         const section = document.getElementById(sectionId);
         if (section instanceof HTMLElement)
           section.scrollIntoView({ behavior: 'smooth', block: position });
